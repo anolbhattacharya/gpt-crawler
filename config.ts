65acb8edebdda13261d94a7cf7d2b10c9a969843
anolbhattacharya <anol@getitcomms.com>
2023-12-01 15:57:15 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,8 +1,9 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
+  url: "https://getit.marketing/our-perspective/",
+  match: "https://getit.marketing/our-perspective/news/**",
+  //selector: `.full-story-wrap`,
   maxPagesToCrawl: 50,
   outputFileName: "output.json",
 };
